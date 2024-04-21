6af2c21e6349712d49d3317de668631a52d248af
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-21 11:01:54 -0600

    feat: particlesJS added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'src/assets/particlesJS/particles.config.json');
+  }, []);
+
   return (
     <>
-      <h1 className="text-red-500 text-4xl">Hello World</h1>
+      <div id="particles-js" className="w-screen h-screen fixed top-0 -z-10"></div>
+      <div className="bg-red-500">
+        <h1>Hello World</h1>
+      </div>
     </>
   )
 }
