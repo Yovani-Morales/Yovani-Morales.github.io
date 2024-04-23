4f0bf0dfe08b49ab7297597d6498ddb3f17c7362
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-23 12:20:49 -0600

    feat:(Button): add target _blank

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ content, url, color='#6127d1' }) => {
   return (
-    <a style={{background: color}} className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4" href={url}><span>{content}</span></a>
+    <a style={{background: color}} className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4" href={url} target="_blank"><span>{content}</span></a>
   )
 }
 
