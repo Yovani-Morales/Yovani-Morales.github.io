30bcfd66f984fd88bb3aca073d6063c8464c31e5
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-26 16:55:48 -0600

    feat!: add prop children and add outline

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ content, url, color='#6127d1' }) => {
+const Button = ({ children, url, color='#6127d1' }) => {
   return (
-    <a style={{background: color}} className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4" href={url} target="_blank"><span>{content}</span></a>
+    <a style={{background: color}} className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 outline outline-2 outline-color-purple" href={url} target="_blank">{children}</a>
   )
 }
 
