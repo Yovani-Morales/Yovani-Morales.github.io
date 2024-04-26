23ad268f457497fdece0f8bf4bcc6a0bd284572d
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-26 17:08:21 -0600

    feat: add prop style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ children, url, color='#6127d1' }) => {
+const Button = ({ children, url, color='#6127d1', style }) => {
   return (
-    <a style={{background: color}} className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 outline outline-2 outline-color-purple" href={url} target="_blank">{children}</a>
+    <a style={{background: color}} className={"inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 outline outline-2 outline-color-purple "+style} href={url} target="_blank">{children}</a>
   )
 }
 
