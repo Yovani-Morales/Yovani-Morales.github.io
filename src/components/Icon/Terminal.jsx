c6b5a795827f21009a34b2beddf67ab2fdd1acb1
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-28 17:10:36 -0600

    feat: create programming language icons

---
+++ src/components/Icon/Terminal.jsx
@@ -0,0 +1,36 @@
+const Terminal = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width={50} height={50} {...props}>
+    <g transform="translate(0 -289.063)">
+      <circle
+        cx={16.607}
+        cy={301.988}
+        r={0}
+        style={{
+          opacity: 1,
+          fill: "#40cd8b",
+          fillOpacity: 1,
+          stroke: "none",
+          strokeWidth: 2,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M5 3c-1.108 0-2 .892-2 2v20c0 1.108.892 2 2 2h20c1.108 0 2-.892 2-2V5c0-1.108-.892-2-2-2H5zm1.875 5.396 6.52 4.026v1.635l-6.47 4.025L6 16.734l5.762-3.486L6 9.795l.875-1.399zm8.703 11.657H24v1.55h-8.422v-1.55z"
+        style={{
+          opacity: 1,
+          fill: "#fff",
+          fillOpacity: 1,
+          stroke: "none",
+          strokeWidth: 2,
+          strokeMiterlimit: 4,
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="translate(0 289.063)"
+      />
+    </g>
+  </svg>
+)
+export { Terminal };
