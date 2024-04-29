d0e85dec6997470079904f7ef60a117b199d2b80
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-28 18:24:08 -0600

    feat(projects): edit title

--- src/components/Main/Projects/Projects.jsx
+++ src/components/Main/Projects/Projects.jsx
@@ -19,9 +19,8 @@ const Projects = () => {
   return (
     <section className="w-3/4 mx-auto">
       <div className="grid grid-cols-6 grid-rows-[repeat(2,_300px)] gap-8">
-        <CardContainer style='col-span-3 p-10 flex flex-col justify-between'>
-          <h2 className="text-7xl font-bold text-color-purple">Alguno De Mis Proyectos</h2>
-          <p className="text-lg">Estos son algunos de mis mejores proyectos que he realizado.</p>
+        <CardContainer style='col-span-3 p-10 flex items-center'>
+          <h2 className="text-[75px] leading-none font-bold text-color-purple">Alguno De Mis Mejores Proyectos</h2>
         </CardContainer>
         <CardContainer style='col-span-3'>
           <IMG src="https://cdna.artstation.com/p/assets/images/images/039/339/494/original/gustavo-pinto-vilas-boas-animmanelean-02.gif" alt="Lofi Gif" />
