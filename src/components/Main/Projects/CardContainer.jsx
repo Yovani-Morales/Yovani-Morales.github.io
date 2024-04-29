2e19d7b7252cf77bf7a74eb9121222c70ff1c521
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2024-04-28 18:14:46 -0600

    feat: articles border radius at 50px

--- src/components/Main/Projects/CardContainer.jsx
+++ src/components/Main/Projects/CardContainer.jsx
@@ -1,6 +1,6 @@
 const CardContainer = ({ children, style }) => {
   return (
-    <article className={"rounded-[60px] border border-color-gray bg-color-very-gray overflow-hidden "+style}>{children}</article>
+    <article className={"rounded-[50px] border border-color-gray bg-color-very-gray overflow-hidden "+style}>{children}</article>
   )
 }
 
--- src/components/Main/Technologies/Article.jsx
+++ src/components/Main/Technologies/Article.jsx
@@ -1,6 +1,6 @@
 const Article = ({ children, className }) => {
   return (
-    <article className={"rounded-[30px] flex flex-col justify-center gap-5 bg-[#1d1c2043] border border-color-gray "+className}>{children}</article>
+    <article className={"rounded-[50px] flex flex-col justify-center gap-5 bg-[#1d1c2043] border border-color-gray "+className}>{children}</article>
   )
 }
 
