9f39b7922289a346ad09fd85a26d71a7bd2185c7
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-05-02 03:01:34 -0600

    feat: use about component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,13 @@
 import { Projects } from "./Projects/Projects";
 import { Technologies } from "./Technologies/Technologies";
+import { About } from "./About/About";
 
 const Main = () => {
   return (
     <main className="py-20 flex flex-col gap-20">
       <Projects />
       <Technologies />
-      <section>Acerca de mi</section>
+      <About />
     </main>
   )
 }
