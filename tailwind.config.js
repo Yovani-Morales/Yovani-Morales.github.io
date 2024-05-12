21c695fa51fe45f6c1ca79147610d2a0c9eb52ae
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-05-12 16:27:43 -0600

    feat: custom breakpoints added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,22 @@ export default {
         'color-gray': '#71767b',
       }
     },
+    screens: {
+      'cs-1420': {max: '1420px'},
+      'cs-1330': {max: '1330px'},
+      'cs-1255': {max: '1255px'},
+      'cs-1185': {max: '1185px'},
+      'cs-1110': {max: '1110px'},
+      'cs-1050': {max: '1050px'},
+      'cs-1000': {max: '1000px'},
+      'cs-950': {max: '950px'},
+      'cs-850': {max: '850px'},
+      'cs-780': {max: '780px'},
+      'cs-659': {max: '659px'},
+      'cs-460': {max: '460px'},
+      'cs-430': {max: '430px'},
+      'cs-400': {max: '400px'},
+    }
   },
   plugins: [],
 }
