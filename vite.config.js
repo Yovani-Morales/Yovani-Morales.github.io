9212c4737afa544d353d956aa666ec9dcf0eca64
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-10-29 12:56:48 -0600

    feat: update base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://yovany-dev.github.io/portafolio/'
+  base: 'yovanymorales.dev'
 })
